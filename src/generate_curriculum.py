a1fb0c38f8a70d7f44aa8f3378abf6c759cda678
gio-del <giovanni.delucia@polimi.it>
2024-04-19 12:57:28 +0200

    multi language

--- src/generate_curriculum.py
+++ src/generate_curriculum.py
@@ -1,28 +1,33 @@
 import json
+import os
 
 import pdfkit
 from jinja2 import Environment, FileSystemLoader
 
-
 def main():
-    with open('data/curriculum.json', 'r') as f:
-        personal_info = json.load(f)
+    for file in os.listdir('data'):
+        if file.endswith('.json'):
+            with open(f'data/{file}', 'r') as f:
+                personal_info = json.load(f)
 
-    env = Environment(loader=FileSystemLoader('templates'))
-    template = env.get_template('curriculum.html')
+            env = Environment(loader=FileSystemLoader('templates'))
+            template = env.get_template('curriculum.html')
 
-    rendered_content = template.render(personal_info)
+            rendered_content = template.render(personal_info)
 
-    with open('temp_cv.html', 'w') as f:
-        f.write(rendered_content)
+            with open('temp_cv.html', 'w') as f:
+                f.write(rendered_content)
 
-    pdfkit.from_file('temp_cv.html', 'output/cv.pdf')
+            if not os.path.exists('output'):
+                os.makedirs('output')
 
-    import os
-    os.remove('temp_cv.html')
+            pdfkit.from_file('temp_cv.html', f'output/{file.replace(".json", ".pdf")}',
+                             options={'page-size': 'Letter', 'margin-top': '0.75in', 'margin-right': '0.75in',
+                                      'margin-bottom': '0.75in', 'margin-left': '0.75in'})
 
-    print("CV generated successfully!")
+            # os.remove('temp_cv.html')
 
+            print(f"CV for {file.replace('.json', '')} generated successfully!")
 
 if __name__ == "__main__":
     main()
